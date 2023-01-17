6c6621d8d576284693c4feaed6834db7b7a56dfa
Juanmi0112 <juanmiguelariasdeleon@gmail.com>
2023-01-17 03:08:23 -0400

    AJUSTES FINALES (FAVOR DE REVISAR EL CODIGO)

--- camara.py
+++ camara.py
@@ -1,4 +1,12 @@
 import cv2
+import telepot
+import keyboard
+import token 
+
+
+def receiver_id():
+
+
 print(cv2.__version__)
 cam=cv2.VideoCapture(0)
 while True:
@@ -21,14 +29,13 @@ with keyboard.GlobalHotKeys(hotkeys) as listener:
     listener.join()
 
 def takePhoto():
-    import cv2
-    import telepot
+   
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
-    cv2.imwrite('c:xxxxxxxxxxxxxxxxxxx,frame)   #colocar direccion de la imagen reemplazar por la x
+    cv2.imshow('C:\Users\juanm\Downloads',frame)   #colocar direccion de la imagen reemplazar por la x
     cap.release()
     bot = telepot.Bot(token)
-    bot.Message(receiver_id,'picture taken.')
-    bot.sendphoto(receiver_id, photo=open('SCREENSHOT', 'rb')) #sustituir rb por frame
+    bot.notifyMessage(receiver_id,'picture taken.')
+    bot.sendPicture(receiver_id, photo=open('SCREENSHOT' + img + 'jpeg', 'rb')) #envia la foto
 
 
